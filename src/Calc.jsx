f7cb22e067ce6dfed3fd3682e2a5c2e651879c41
The-CerealDev <ajedavid1@gmail.com>
2025-12-10 18:43:04 +0000

    Starting Commit

--- src/Calc.jsx
+++ src/Calc.jsx
@@ -1,9 +1,49 @@
+import { useState } from "react";
+
+
+function CalcResults(){
+  return(
+    <>
+      <h2>
+        Results
+      </h2>
+      <p>Monthly Mortgage = {100}</p>
+    </>
+  );
+}
 function CalcInput() {
+  const [range, setRange] = useState(0);
+
+  function handleSliderChange(e){
+    
+    e.target.value !== '' ?setRange(e.target.value):setRange(0);
+}
+
   return (
-    <form action="">
-      <label htmlFor="" className=""></label>
-      <input></input>
-    </form>
+    <>
+      <form action="" className="CalcInput">
+        <label htmlFor="" className="">
+          Mortgage Amount:
+          <input type="" placeholder={0} value={range} onChange={handleSliderChange}/>
+          <input placeholder={0} type="range" value={range} id="mortAmountSlider" min={1} max={1000000} onChange={handleSliderChange}/>
+        </label>
+  
+        <label htmlFor="" className="">
+          Term in years:<input />
+          <input type="range" id="mortAmountSlider" min={0} max={1000000} />
+
+        </label>
+
+        <label htmlFor="" className="">
+          
+          Interest Rate:<input/>
+
+          <input type="range" id="mortAmountSlider" min={0} max={1000000} />
+        </label>
+      </form>
+      
+      <CalcResults />
+    </>
   );
 }
 
---
+++ src/Navbar.jsx
@@ -0,0 +1,20 @@
+function Logo(){
+    return(
+        <div className="Logo">
+            <h1>Mortgage Calc</h1>
+        </div>
+    );
+}
+
+function Navbar(){
+
+    return(
+        <>
+            <div className="Navbar">
+                <Logo/> 
+            </div>
+        </>
+    );
+}
+
+export default Navbar;
